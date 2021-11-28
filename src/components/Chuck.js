9f477d935508c24debd78935d176038994aa940c
ToineBOSS <45557751+ToineBOSS@users.noreply.github.com>
2021-11-28 15:39:12 +0100

    Chucl

--- src/components/Chuck.js
+++ src/components/Chuck.js
@@ -1,6 +1,6 @@
 import React , {useState}from 'react';
 import axios from 'axios';
-import "../pages/Accueil.css"
+import "../pages/Accueil.css";
 
 const Chuck = () => {
     const [info, setInfo] = useState("");
