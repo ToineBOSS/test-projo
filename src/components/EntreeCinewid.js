bd589262aef0c540f201460219a4cbbce5ff8456
theootop <90248400+theootop@users.noreply.github.com>
2021-12-08 08:38:07 +0100

    changements mineurs

--- src/components/EntreeCinewid.js
+++ src/components/EntreeCinewid.js
@@ -1,5 +1,5 @@
 import '../pages/Accueil.css'
-import React , {useState}from 'react';
+import React , {useState, useEffect}from 'react';
 import Axios from 'axios';
 
 
@@ -15,8 +15,17 @@ const addTolist = () => {
 };
 const[NbAnnee, setNbAnnee] = useState('')
 const[NbTotEntrees, setNbTotEntrees] = useState(0);
+const[AnneeList, setAnneeList] = useState([]);
+useEffect(()=> {
+
+    Axios.get("http://localhost:3002/entrees").then((response) => {
+        setAnneeList(response.data);
+    });
+},[]
+);
+
 return (
-    
+    <div>
     <div className="Crud">
                 <div className="CRUDTITLE">ADMIN MODE</div>
                 <label>Année:</label>
@@ -28,6 +37,19 @@ return (
                 <button className="btn_crud" onClick={addTolist} >Ajouter a la liste </button>
                 
     </div>
+
+    <div className="Crud">Liste des données</div>
+{AnneeList.map((val,key) => {
+    return 
+        
+       <div>
+        <h2> {val.NbAnnee} </h2><h2> {val.NbTotentree} </h2>
+        </div>
+        
+}
+)
+}
+</div>
 
 )};
 
--- src/pages/News.js
+++ src/pages/News.js
@@ -33,7 +33,7 @@ const News = () => {
 
         
 
-        <Carou/>
+        
         <Graphe/>
         </Row>
         </Container>
